dd0ca9a768debd8446bbb86eca45b93315478b00
Frank Moreno <frankmoreno1993@gmail.com>
2017-11-19 13:31:19 -0500

    proj03: smartPresencia

---
+++ proyectos/03/smartPresencia/presencia.cpp
@@ -0,0 +1,30 @@
+#include "smartConfig.h"
+
+const int trigger = D6;
+const int echo = D7;
+
+// sala.pres
+Adafruit_MQTT_Subscribe presReq = Adafruit_MQTT_Subscribe(&mqtt, "sala/pres/req");
+Adafruit_MQTT_Publish presSta = Adafruit_MQTT_Publish(&mqtt, "sala/pres/sta");
+
+void presenciaSetup(){
+  pinMode(trigger,OUTPUT);
+  pinMode(echo,INPUT);
+  
+  presReq.setCallback(presenciaCallback);
+  mqtt.subscribe(&presReq);
+}
+
+void presenciaCallback(char *payload, uint16_t len){
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+  
+  long tiempo = (pulseIn(echo, HIGH)/2);
+  float distancia = float(tiempo * 0.0343);
+  
+  Serial.println(distancia);
+
+  presSta.publish(distancia);
+}
+
---
+++ proyectos/03/smartPresencia/smartConfig.cpp
@@ -0,0 +1,45 @@
+#include "smartConfig.h"
+
+WiFiClient wifi;
+Adafruit_MQTT_Client mqtt(&wifi, MQTT_SERVER, MQTT_PORT, MQTT_USER, MQTT_USER, MQTT_PASS);
+
+void wifiSetup(){
+  Serial.println(); Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(WLAN_SSID);
+
+  WiFi.begin(WLAN_SSID, WLAN_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.println("WiFi connected");
+  Serial.println("IP address: "); Serial.println(WiFi.localIP());
+}
+
+void MQTT_connect() {
+  int8_t ret;
+
+  // Stop if already connected.
+  if (mqtt.connected()) {
+    return;
+  }
+
+  Serial.print("Connecting to MQTT... ");
+
+  uint8_t retries = 3;
+  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+       Serial.println(mqtt.connectErrorString(ret));
+       Serial.println("Retrying MQTT connection in 10 seconds...");
+       mqtt.disconnect();
+       delay(10000);  // wait 10 seconds
+       retries--;
+       if (retries == 0) {
+         // basically die and wait for WDT to reset me
+         while (1);
+       }
+  }
+  Serial.println("MQTT Connected!");
+}
