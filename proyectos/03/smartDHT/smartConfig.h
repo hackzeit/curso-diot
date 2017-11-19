a9d927a71a7658be88039ff60e300fbe8d913e6f
Frank Moreno <frankmoreno1993@gmail.com>
2017-11-19 10:29:43 -0500

    proj03: simple smartDHT

---
+++ proyectos/03/smartDHT/smartConfig.h
@@ -0,0 +1,32 @@
+#include <ESP8266WiFi.h>
+#include "Adafruit_MQTT.h"
+#include "Adafruit_MQTT_Client.h"
+
+/********** WiFi Access Point ***********/
+#define WLAN_SSID       "network"
+#define WLAN_PASS       "password"
+
+/********** MQTT Broker *****************/
+#define MQTT_SERVER "m14.cloudmqtt.com"
+#define MQTT_PORT   1883
+#define MQTT_USER   "user"
+#define MQTT_PASS   "pass"
+
+/********** DHT *****************/
+#define DHTTYPE DHT11
+
+/************ clientes wifi y mqtt*************/
+extern WiFiClient wifi;
+extern Adafruit_MQTT_Client mqtt;
+
+void wifiSetup();
+void MQTT_connect();
+
+/***************** Domotica ******************/
+
+// DHT - Humedad y Temperatura
+void dhtSetup();
+void dhtCallback(char *payload, uint16_t len);
+
+
+
---
+++ proyectos/03/smartDHT/smartDHT.ino
@@ -0,0 +1,19 @@
+#include "smartConfig.h"
+
+void setup() {
+  Serial.begin(115200);
+  delay(10);
+
+  Serial.println(F("Projecto 03: Smart sensor de temperatura"));
+
+  wifiSetup();
+  dhtSetup(); // inicializa el dht y configura la subscricion a mqtt
+}
+
+void loop() {
+  MQTT_connect();
+  mqtt.processPackets(10000);
+  if(! mqtt.ping()) {
+    mqtt.disconnect();
+  }
+}
